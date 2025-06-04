845d022bfb51c62b4bf8839136cb2673ddb0c134
jeseeqing <jeseeqing@gmail.com>
2025-06-04 18:00:09 +0800

    feat: add GetFilterValueAsObjectId func for SearchInput structure

--- controllerx/search_input.go
+++ controllerx/search_input.go
@@ -4,6 +4,7 @@ import (
 	"github.com/abmpio/entity"
 	"github.com/abmpio/webserver/controller"
 	"github.com/kataras/iris/v12"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type SearchInput struct {
@@ -13,7 +14,30 @@ type SearchInput struct {
 	SortInput
 }
 
+// 获取指定的key的primitive.ObjectID值
+func (i *SearchInput) GetFilterValueAsObjectId(key string) (*primitive.ObjectID, error) {
+	if len(i.Filter) <= 0 {
+		return nil, nil
+	}
+	v, ok := i.Filter[key].(string)
+	if !ok {
+		return nil, nil
+	}
+	if len(v) <= 0 {
+		return nil, nil
+	}
+	oid, err := primitive.ObjectIDFromHex(v)
+	if err != nil {
+		return nil, err
+	}
+	return &oid, nil
+}
+
+// 获取指定的key的string值
 func (i *SearchInput) GetFilterValueAsString(key string) string {
+	if len(i.Filter) <= 0 {
+		return ""
+	}
 	v, ok := i.Filter[key].(string)
 	if ok {
 		return v
